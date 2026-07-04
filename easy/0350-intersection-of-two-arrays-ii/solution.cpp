634c965bb2013c0149c41d0d4f5ccb939a7dbd73
krish sharma <sharmakrish0761@gmail.com>
2026-07-04 13:27:12 +0530

    ✅ Intersection of Two Arrays II | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-07-04

--- easy/0350-intersection-of-two-arrays-ii/solution.cpp
+++ easy/0350-intersection-of-two-arrays-ii/solution.cpp
@@ -1,16 +1,29 @@
-class Solution {
-public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-   unordered_set<int>st(nums1.begin(),nums1.end());
-    unordered_set<int>result;
-    for(int i=0;i<nums2.size();i++){
-         if(st.find(nums2[i])!=st.end())
-          result.insert(nums2[i]);
-    }
-    vector<int>ans(result.begin(),result.end());
-    return ans;
-        
+        vector<int>inter;
+        int n1 = nums1.size();
+        int n2= nums2.size();
+        int i=0,j=0;
+        while(i<n1&&j<n2){
+             if(nums1[i]==nums2[j]){
+                if(inter.size()==0||inter.back()!=nums1[i]){
+                     inter.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+
+             }
+             else if(nums1[i]<nums2[j]){
+                 i++;
+             }
+             else {
+              j++;  // this  one haing nlogn+mlogn
+             }
+        }
+        return inter;
 
    
     }
-};
+        sort(nums2.begin(),nums2.end());
+     sort(nums1.begin(),nums1.end());
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+class Solution {
+public:
