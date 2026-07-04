26c7404ce7ad2b526a7197ce6298807a4fede9a7
krish sharma <sharmakrish0761@gmail.com>
2026-07-04 16:30:48 +0530

    ✅ Longest Consecutive Sequence | Medium | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-07-04

--- medium/0128-longest-consecutive-sequence/solution.cpp
+++ medium/0128-longest-consecutive-sequence/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] - nums[i] == -1) {
-                count++;
-            } else if (nums.empty())
-                return 0;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]-nums[i]==-1){
+                 count++;
+            }
+
+             
         }
 
-        return count+1;
+        return count;
     }
 };
