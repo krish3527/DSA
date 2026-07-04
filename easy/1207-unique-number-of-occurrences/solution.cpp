f9113da47d9d07b95f845f3042052d6ebb688ad0
krish sharma <sharmakrish0761@gmail.com>
2026-07-04 13:55:11 +0530

    ✅ Unique Number of Occurrences | Easy | C++ | Runtime: 0 ms | Memory: 8.3 MB | 2026-07-04

--- easy/1207-unique-number-of-occurrences/solution.cpp
+++ easy/1207-unique-number-of-occurrences/solution.cpp
@@ -1,19 +1,19 @@
-class Solution {
+class Solution {
 public:
-    bool uniqueOccurrences(vector<int>& arr) {
-        int n = arr.size();
-        for(int i=0;i<n;i++){
-        }
-        
-        unordered_map<int,int>mpp;
-            mpp[arr[i]]++;   
-        for(int i=0;i<mpp.size();i++){
-             for(int j=i+1;j<mpp.size()-1;j++){
-        }
-                 if(mpp[i]!=mpp[j]){
-             }
-                     return true;
-                 }
-    }
-        return false;
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[arr[i]]++;   
+        }
+        for(int i=0;i<mpp.size();i++){
+             for(int j=i+1;j<mpp.size()-1;j++){
+                 if(mpp[i]!=mpp[j]){
+                     return true;
+                 }
+             }
+        }
+        return false;
+        
+    }
 };
