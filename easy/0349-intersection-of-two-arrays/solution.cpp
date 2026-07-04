6bede6fb587f17093a60c5c1460d9e5456e3b406
krish sharma <sharmakrish0761@gmail.com>
2026-07-04 12:53:01 +0530

    ✅ Intersection of Two Arrays | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 13.9 MB | 2026-07-04

--- easy/0349-intersection-of-two-arrays/solution.cpp
+++ easy/0349-intersection-of-two-arrays/solution.cpp
@@ -1,29 +1,29 @@
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        vector<int>inter;
-        int n1 = nums1.size();
-        int n2= nums2.size();
-        int i=0,j=0;
-        while(i<n1&&j<n2){
-             if(nums1[i]==nums2[j]){
-        }
-                if(inter.size()==0||inter.back()!=nums1[i]){
-             }
-                     inter.push_back(nums1[i]);
-                }
-                i++;
-                j++;
-
-             else if(nums1[i]<nums2[j]){
-                 i++;
-             }
-
-             else {
-              j++;
-        return inter;
-    }
-             }
-};
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+class Solution {
 public:
-class Solution {
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int>inter;
+        int n1 = nums1.size();
+        int n2= nums2.size();
+        int i=0,j=0;
+        while(i<n1&&j<n2){
+             if(nums1[i]==nums2[j]){
+                if(inter.size()==0||inter.back()!=nums1[i]){
+                     inter.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+
+             }
+             else if(nums1[i]<nums2[j]){
+                 i++;
+             }
+             else {
+              j++;
+             }
+        }
+        return inter;
+    }
+
+};
