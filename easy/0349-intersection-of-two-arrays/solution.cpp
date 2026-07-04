858c08d12ef307f87ec4c49114dc6f846f2d6f6c
krish sharma <sharmakrish0761@gmail.com>
2026-07-04 11:35:04 +0530

    ✅ Intersection of Two Arrays | Easy | C++ | Runtime: 0 ms | Memory: 8.4 MB | 2026-07-04

---
+++ easy/0349-intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,25 @@
+        int n1 = nums1.size();
+        int n2= nums2.size();
+        int i =0;
+        while(i<n1&&j<n2){
+        
+             if(nums1[i]==nums2[j]){
+        }
+                 
+            
+                 if(inter.size()==0||inter.back()!=nums1[i]){
+                     inter.push_back(nums1[i]);
+                 }
+            i++;
+
+            j++;
+            else if(nums1[i]<nums2[j]){
+                 i++;
+            }
+    }
+            else 
+            j++;
+        return inter;
+        int j=0;
+             }
+};
