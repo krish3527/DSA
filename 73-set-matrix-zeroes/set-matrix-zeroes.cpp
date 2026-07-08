a2595b9aba1d67438d0c7b3fc941ab89b7a2bbe8
krish sharma <sharmakrish0761@gmail.com>
2026-07-08 14:29:36 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (50.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,7 +5,6 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
        /* BETTER WITH TC O(N*M);
-
         vector<int>coln(n,0);
         vector<int>row(m,0);
         for (int i = 0; i < m; i++) {
@@ -15,7 +14,7 @@ public:
                     row[i] = 1;
                 } 
             }
-        }
+        }  
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if(row[i]||coln[j]){
