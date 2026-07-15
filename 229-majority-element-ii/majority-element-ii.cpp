f1c9d5280efce256fa871df836967559f4dea00f
krish sharma <sharmakrish0761@gmail.com>
2026-07-15 10:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (65.70%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans;
+    /*    vector<int>ans;
      map<int,int>mpp;
      int mini = n/3+1;
      for(int i=0;i<n;i++){
@@ -13,5 +13,37 @@ public:
          }
      }
      return ans ;
+    }
+};*/
+int count1=0,count2=0;
+int el1=0,el2=0;
+for(int i=0;i<n;i++){
+     if(count1==0&&nums[i]!=el2){
+         count1=1;
+         el1=nums[i];
+     }
+     else if(count2==0&&nums[i]!=el1){
+         count2=1;
+         el2=nums[i];
+     }
+     else if(el1==nums[i]) count1++;
+     else if(el2==nums[i]) count2++;
+     else{ 
+     count1--;
+     count2--;
+     }
+}
+int c1=0,c2=0;
+for(int i=0;i<n;i++){
+     if(el1==nums[i])c1++;
+     else if(el2==nums[i])c2++;
+}
+int mini = (int)(n/3)+1;
+vector<int>ans;
+if(c1>=mini)
+ ans.push_back(el1);
+ if(c2>=mini)
+  ans.push_back(el2);
+  return ans;
     }
 };
