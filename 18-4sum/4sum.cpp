4e0d312928e968529bd3a45cc31a1e78f8682ddb
krish sharma <sharmakrish0761@gmail.com>
2026-07-26 16:38:10 +0530

    Time: 22 ms (59.90%) | Memory: 17.6 MB (23.65%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -7,8 +7,7 @@ public:
        for(int i=0;i<n;i++){
         if(i>0&&nums[i]==nums[i-1])continue;
            for(int j=i+1;j<n;j++){
-            if(j > i+1 && nums[j]==nums[j-1])
-    continue;
+            if(j > i+1 && nums[j]==nums[j-1]) continue;
              int left = j+1;
              int right = n-1;
              while(left<right){
