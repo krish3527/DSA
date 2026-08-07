990528c9baca591564aec737dd7a707fd54ab231
krish sharma <sharmakrish0761@gmail.com>
2026-08-07 23:15:25 +0530

    Time: 1 ms (51.44%) | Memory: 26.2 MB (53.33%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,21 +2,19 @@
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
-        vector<int> hash(n+1,0);
-
+        vector<int>hash(n+1,0);
+      int missing=-1;
+      int repeating=-1;
         for(int i=0;i<n;i++){
             hash[nums[i]]++;
         }
         for(int i=1;i<=n;i++){
             if(hash[i]==2)
-                ans.push_back(i);
-        }
-        for(int i=1;i<=n;i++){
+               repeating =i;
             if(hash[i]==0)
-                ans.push_back(i);
+               missing =i;
         }
 
-        return ans;
+        return {repeating ,missing};
     }
 };
