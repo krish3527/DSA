b4d4019812db5d9f89893c040bba177e0aa7f1a5
krish sharma <sharmakrish0761@gmail.com>
2026-08-12 00:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (88.36%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,8 +1,8 @@
 ;class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>hash(n+1,0);
+        long long  n = nums.size();
+       /* vector<int>hash(n+1,0);
       int missing=-1;
       int repeating=-1;
         for(int i=0;i<n;i++){
@@ -16,5 +16,21 @@ public:
         }
 
         return {repeating ,missing};
+    }*/
+    long long  sum1= (n*(n+1))/2;
+    long long  sum2 =(n*(n+1)*(2*n+1))/6;
+    long long sumofarr=0;
+    long long sumofsquare=0;
+    for(int i=0;i<n;i++){
+        sumofarr+=nums[i];
+        sumofsquare+=(long long)nums[i]*nums[i];
     }
+    long long  val1=sumofarr-sum1;//x-y
+    long long  val2=sumofsquare-sum2;
+    val2=val2/val1;//x+y
+    long long  x = (val1+val2)/2;
+    long long  y = x-val1;
+    return {(int)x,(int)y};
+    }
+
 };
